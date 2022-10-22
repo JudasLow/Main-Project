07a5023138e4b7a3b0594c9a9c6fff61611bbef0
iskubee <iskandershah_ss43@hotmail.com>
2022-10-22 13:59:37 +0800

    test

--- login.js
+++ login.js
@@ -17,11 +17,13 @@ function getInfo() {
 	var username = document.getElementById('username').value
 	var password = document.getElementById('password').value
 
+	console.log("getInfo is called")
+
 	for(var i = 0; i < objPeople.length; i++) {
 		// check is user input matches username and password of a current index of the objPeople array
 		if(username == objPeople[i].username && password == objPeople[i].password) {
 			console.log(username + " is logged in!!!")
-            location.replace("https://www.w3schools.com")
+            location.replace("TEST")
 			// stop the function if this is found to be true
 			return
 		}
