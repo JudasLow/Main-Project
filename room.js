4352ac2d82785650e8ef699720052f7024b4cea7
JudasLow <56168967+JudasLow@users.noreply.github.com>
2022-10-31 23:38:04 +0800

    HH

--- room.js
+++ room.js
@@ -10,29 +10,34 @@ price = document.getElementById("price")
 jsonBtn = document.getElementById("jsonbtn")
 jsonText = document.getElementById("jsontext")
 
-display = document.getElementById("Display Item")
+displayRoomsBtn = document.getElementById("displayRooms1")
+displayTable = document.getElementById("displayroom")
 
 //add click event listener, to get data when data is entered
 jsonBtn.addEventListener
 ("click", function(test)
   {
+
     //store data in JavaScript object
-      var room = 
-      {
-          "rid":id.value,
-          "roomname":roomname.value,
-          "capacity":capacity.value,
-          "price":price.value
-      }
+    // COMMENT1: hello, you are storing only one object and replacing that one object in localstorage each time you are doing this
+    // COMMENT2: I help you to store it in JSON as array so you can figure out how to add on in future in COMMENT2
+    var room = 
+    [{
+        "rid":id.value,
+        "roomname":roomname.value,
+        "capacity":capacity.value,
+        "price":price.value
+    }]
 
-      //convert JavaScript object to JSON
-      jsonText.innerText = JSON.stringify(room)
-      
-      localStorage.setItem(id.value,jsonText.innerText)
+    //convert JavaScript object to JSON
+    jsonText.innerText = JSON.stringify(room)
+    
+    // COMMENT3: you need to change the logic here to add on to what is in the local storage, not just replace iot
+    localStorage.setItem(id.value,jsonText.innerText)
 
-      var test = jsonText.innerText;
+    var test = jsonText.innerText;
 
-      return test;
+    return test;
   }                     
 )
 
@@ -40,21 +45,31 @@ jsonBtn.addEventListener
 
 
 //Currently working on this 30/10/2022
-$(document).ready(function () {
-  var json =  JSON.parse(localStorage.getItem(id.value,jsonText.innerText));  //[{"User_Name":"John Doe","score":"10","team":"1"}, {"User_Name":"Jane Smith","score":"15","team":"2"},{"User_Name":"Chuck Berry","score":"12","team":"2"}];
+displayRoomsBtn.addEventListener("click", function (){
+  var info = JSON.parse(localStorage.getItem(id.value,jsonText.innerText))
+  console.log("displaying");
+  console.log(info)
+  console.log(info.length)
+  //[{"User_Name":"John Doe","score":"10","team":"1"}, {"User_Name":"Jane Smith","score":"15","team":"2"},{"User_Name":"Chuck Berry","score":"12","team":"2"}];
+
+
+  for (var key in info) {
+    console.log("testing");
+    console.log(key)
+    console.log(info[key])
   
+    // For each item, create a new row
+    var newRow = displayTable.insertRow()
 
-  
-    var tr;
+    // For each row, add the columns
+    for (var key2 in info[key]) {
+      var firstCell = newRow.insertCell()
+      var firstCellText = document.createTextNode(info[key][key2]) // create a text node here so text
+      firstCell.appendChild(firstCellText)
+    }
 
+  }    
 
-  for (var i = 0; i < json.length; i++) {
-          tr = $('<tr/>');
-          tr.append("<td>" + json[i].User_Name + "</td>");
-          tr.append("<td>" + json[i].roomname + "</td>");
-          tr.append("<td>" + json[i].capacity + "</td>");
-          $('table').append(tr);
-      }
 });
 
 
